d3f62a08ddfb5d43b29f3d832ea8454db7eca631
Leonardo Meléndez <leogatgens@gmail.com>
2018-12-18 16:45:22 -0600

    Ajustes

--- leoapp/src/components/menu.js
+++ leoapp/src/components/menu.js
@@ -38,7 +38,7 @@ import {
                 <NavLink exact to="/" className="nav-link" activeclassname="active" tag={RRNavLink}>Home</NavLink>
             </NavItem>
             <NavItem>
-                <NavLink actvieclassname="active" to="/marcas" tag={RRNavLink}>  Marcas de autos </NavLink>
+                <NavLink actvieclassname="active" to="/marcas" tag={RRNavLink}>Por visitar </NavLink>
             </NavItem>
             <NavItem>              
                 <NavLink actvieclassname="active" to="/misviajes" tag={RRNavLink}> Mis viajes</NavLink>             
