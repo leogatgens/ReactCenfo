515ffb1415b018a7007c21ddd948809fb6f2edd0
Leonardo Meléndez <leogatgens@gmail.com>
2018-12-20 11:57:10 -0600

    ajustes

--- leoapp/src/components/timeline.js
+++ leoapp/src/components/timeline.js
@@ -9,8 +9,16 @@ class PendingTimeLine extends React.Component {
       <div>
         <Timeline >
           <Timeline.Item>Tu primer viaje Venezuela 2015-09-01</Timeline.Item>
-          <Timeline.Item dot={<Icon type="clock-circle-o" style={{ fontSize: '16px' }} />} color="red">Technical testing 2015-09-01</Timeline.Item>
-          <Timeline.Item>Technical testing 2015-09-01</Timeline.Item>
+          <Timeline.Item dot={<Icon type="clock-circle-o" style={{ fontSize: '16px' }} />} color="red">
+          Tu primer viaje a Cuba 2015-09-01          
+          </Timeline.Item>
+          <Timeline.Item>Tu viaje Nardia 2015-09-01</Timeline.Item>
+          <Timeline.Item>Tu viaje Nardia 2015-09-01</Timeline.Item>
+          <Timeline.Item>Tu viaje Nardia 2015-09-01</Timeline.Item>
+          <Timeline.Item>Tu viaje Nardia 2015-09-01</Timeline.Item>
+          <Timeline.Item>Tu viaje Nardia 2015-09-01</Timeline.Item>
+          <Timeline.Item>Tu viaje Nardia 2015-09-01</Timeline.Item>          
+          <Timeline.Item>Tu último viaje a SF USA 2015-09-01</Timeline.Item>
         </Timeline>
         
       </div>
