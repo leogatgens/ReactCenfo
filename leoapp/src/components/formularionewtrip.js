c2e7275152a8b88323e09777947311e45e7af7f0
Leonardo Meléndez <leogatgens@gmail.com>
2019-01-07 15:43:56 -0600

    cambio para seleccionar paises

--- leoapp/src/components/formularionewtrip.js
+++ leoapp/src/components/formularionewtrip.js
@@ -1,14 +1,17 @@
 import React from 'react';
 import {TomatoButton,Wrapper} from '../estilos';
 import { TituloPrincipal } from './estiloshtml';
+import { SelectCountry } from './selectcountry';
 
 
  const Formularionewtrip = (props) => {
 
     return( 
         <Wrapper>
-            <TituloPrincipal>Paises por visitar</TituloPrincipal>
-            <TomatoButton>+ Nuevo lugar</TomatoButton>
+
+            <TituloPrincipal>Seleccione sus nuevos países por visitar</TituloPrincipal>
+            <SelectCountry></SelectCountry>
+            <TomatoButton>+ Agregar países</TomatoButton>
         </Wrapper>
     );
 }
